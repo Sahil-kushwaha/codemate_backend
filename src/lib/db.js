3db5513af6edc0806077230077d85d13f3d34769
sahil kushwaha <sahilkushwaha5510@gmail.com>
2026-03-28 03:37:43 +0530

    fix/DB env

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,10 +4,11 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    if (ENV.PROD_DB_URL) {
+    if (!ENV.DB_URL) {
       throw new Error("DB_URL is not defined in environment variables");
     }
-    const conn = await mongoose.connect( ENV.PROD_DB_URL );
+    console.log(ENV);
+    const conn = await mongoose.connect( ENV.DB_URL );
     console.log("✅ Connected to MongoDB:", conn.connection.host);
   } catch (error) {
     console.error("❌ Error connecting to MongoDB", error);
